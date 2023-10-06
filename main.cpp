a9dfbccae165f065b72a8babc60e544217e2d403
disha-baghel <bagheldisha708@gmail.com>
2023-10-07 01:31:16 +0530

    main code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "PixPro.h"
 
-int main () {
+int main (int argc, char* argv[]) {
 
     TGA::Header header;
     
-    TGA::load_header("./Images/landscape.tga", header);
+    TGA::load_header("./Images/dragon.tga", header);
 
     TGA::show_header(header);
 
-    TGA::read_pixel_data("./Images/landscape.tga", header);
+    auto image = TGA::read_pixel_data("./Images/dragon.tga", header);
+
+    TGA::show_pixel_data(header, image);
 
     return 0;
 } 
